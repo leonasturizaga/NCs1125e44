fe2acc55ad120705ad7969868ac1f7d2bab16f0f
Mateo P. <mateo.p.giacosa@gmail.com>
2025-11-21 15:28:25 -0300

    feat: router de testimonios

--- backend/src/controllers/testimonies/editTestimony.js
+++ backend/src/controllers/testimonies/editTestimony.js
@@ -2,26 +2,32 @@ const { testimony } = require("../../db");
 
 const editTestimony = async (id, updates) => {
   try {
-    const foundTestimony = await testimony.findByIdAndUpdate(id, updates, {
-      new: true,
-      runValidators: true,
-    });
-
+    const foundTestimony = await testimony.findByPk(id);
     if (!foundTestimony) {
       return {
-        success: true,
-        message: "Testimonio no encontrado",
+        success: false,
+        message: "No se encontró el testimonio",
       };
     }
 
+    Object.entries(updates).forEach(([key, value]) => {
+      if (foundTestimony.dataValues.hasOwnProperty(key)) {
+        foundTestimony[key] = value;
+      }
+    });
+
+    await foundTestimony.save();
+
     return {
       success: true,
-      message: "Testimonio actualizado con exito",
+      message: "Testimonio actualizado con éxito",
+      data: foundTestimony,
     };
   } catch (error) {
     return {
       success: false,
       message: "Error al intentar actualizar el testimonio",
+      messageError: error.message,
     };
   }
 };
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,7 +2,9 @@ const { Router } = require("express");
 const router = Router();
 
 const authRouter = require("../routes/auth/authRouter");
+const testimonyRouter = require("../routes/testimonies/testimoniesRouter");
 
 router.use("/auth", authRouter);
+router.use("/testimonies", testimonyRouter);
 
 module.exports = router;
