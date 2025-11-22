7149ebcc212ba9e6aa1d3eef07b994f4e8f1b760
Mateo P. <mateo.p.giacosa@gmail.com>
2025-11-21 21:11:21 -0300

    Feat: ruta getAll para los testimonios con filtro de pagina y tamaño

--- backend/src/routes/testimonies/testimoniesRouter.js
+++ backend/src/routes/testimonies/testimoniesRouter.js
@@ -3,11 +3,13 @@ const router = Router();
 
 const postTestimonyHandler = require("../../handlers/testimonies/postTestimonyHandler");
 const getTestimonyByIdHandler = require("../../handlers/testimonies/getTestimonyByIdHandler");
+const getAllTestimoniesHandler = require("../../handlers/testimonies/getAllTestimoniesHandler");
 const editTestimonyHandler = require("../../handlers/testimonies/editTestimonyHandler");
 const deleteTestimonyHandler = require("../../handlers/testimonies/deleteTestimonyHandler");
 
 router.post("/post", postTestimonyHandler);
 router.get("/getById/:id", getTestimonyByIdHandler);
+router.get("/getAll", getAllTestimoniesHandler);
 router.put("/edit/:id", editTestimonyHandler);
 router.delete("/delete", deleteTestimonyHandler);
 
