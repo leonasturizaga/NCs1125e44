63580494675ff63ba6711653355f58e32eabaa42
leonAsturizaga <leonasturizaga@gmail.com>
2025-12-02 13:25:27 -0300

    testDeploy backend temp01

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,21 +1,4 @@
 //------------- original backend/src/app.js  ----------------
-// const express = require('express');
-// const cors = require('cors');
-// const morgan = require('morgan');
-
-// const app = express();
-// const routes = require("./routes/index");
-
-// //middlewares
-// app.use(cors());
-// app.use(morgan('dev'));
-// app.use(express.json());
-
-// app.use("/", routes);
-
-// module.exports = app;
-
-//------------- testDeploy in vercel  backend/src/app.js  ----------------
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
@@ -24,13 +7,30 @@ const app = express();
 const routes = require("./routes/index");
 
 //middlewares
-app.use(cors({
-  origin: "https://testimonial-cms-e44.vercel.app",  // ← tu frontend
-  credentials: true,
-}));
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
 app.use("/", routes);
 
 module.exports = app;
+
+//------------- testDeploy in vercel  backend/src/app.js  ----------------
+// const express = require('express');
+// const cors = require('cors');
+// const morgan = require('morgan');
+
+// const app = express();
+// const routes = require("./routes/index");
+
+// //middlewares
+// app.use(cors({
+//   origin: "https://testimonial-cms-e44.vercel.app",  // ← tu frontend
+//   credentials: true,
+// }));
+// app.use(morgan('dev'));
+// app.use(express.json());
+
+// app.use("/", routes);
+
+// module.exports = app;
