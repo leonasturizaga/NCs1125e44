9bde007f52a7c6ef42aa5b0d194f47f07799c955
Mateo P. <mateo.p.giacosa@gmail.com>
2025-12-02 18:56:19 -0300

    chore: agregar seguridad de rutas para usuarios (jwt y rol)

--- backend/src/handlers/testimonies/editTestimonyHandler.js
+++ backend/src/handlers/testimonies/editTestimonyHandler.js
@@ -4,7 +4,8 @@ module.exports = async (req, res) => {
   try {
     const { id } = req.params;
     const updates = req.body;
-    const data = await editTestimonyController(id, updates);
+    const userId = req.user.id
+    const data = await editTestimonyController(id, updates, userId);
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({
--- backend/src/routes/users/usersRouter.js
+++ backend/src/routes/users/usersRouter.js
@@ -1,15 +1,16 @@
 const { Router } = require("express");
 const router = Router();
+const tokenValidate = require("../../middleware/tokenValidate");
 
 const getAllUsersHandler = require("../../handlers/users/getAllUsersHandler");
 const getUserByIdHandler = require("../../handlers/users/getUserByIdHandler");
 const deactivateUserHandler = require("../../handlers/users/deactivateUserHandler");
 const editUserHandler = require("../../handlers/users/editUserHandler");
 
-router.get("/getAll", getAllUsersHandler);
+router.get("/getAll", tokenValidate, getAllUsersHandler);
 router.get("/getById/:id", getUserByIdHandler);
 
-router.put("/deactivate/:id", deactivateUserHandler);
-router.put("/edit/:id", editUserHandler);
+router.put("/deactivate/:id", tokenValidate, deactivateUserHandler);
+router.put("/edit/:id", tokenValidate, editUserHandler);
 
 module.exports = router;
