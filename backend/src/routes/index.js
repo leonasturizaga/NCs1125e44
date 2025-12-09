504ecf5aca5f92da347d917c01c465498c7fed70
Florencia Lavie <florlavie13@gmail.com>
2025-12-09 14:49:26 -0300

    feat: embeds

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -5,10 +5,14 @@ const authRouter = require("../routes/auth/authRouter");
 const testimonyRouter = require("../routes/testimonies/testimoniesRouter");
 const usersRouter = require("../routes/users/usersRouter");
 const categoriesRouter = require("../routes/categories/categoriesRouter");
+//embeds routes
+const embedRoutes = require("../routes/embedRoutes");
 
 router.use("/auth", authRouter);
 router.use("/testimonies", testimonyRouter);
 router.use("/users", usersRouter);
 router.use("/categories", categoriesRouter);
+//embeds
+router.use("/", embedRoutes);
 
 module.exports = router;
